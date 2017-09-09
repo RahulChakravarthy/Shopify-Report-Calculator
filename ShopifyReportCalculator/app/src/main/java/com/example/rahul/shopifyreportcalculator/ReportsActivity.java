296e67e120f6145c2fe39d15b43dfcd6341b8d97
Rahul-Chakravarthy <rahul.chakravarthy101@gmail.com>
2017-09-09 12:37:13 -0400

    Created app challenge repo

---
+++ ShopifyReportCalculator/app/src/main/java/com/example/rahul/shopifyreportcalculator/ReportsActivity.java
@@ -0,0 +1,13 @@
+package com.example.rahul.shopifyreportcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reports);
+    }
+}
